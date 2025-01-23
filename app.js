2ea35f4363cdefa48fe50071e1a7e993d7f0f497
Camila Caetano <82657196+camilacaetano@users.noreply.github.com>
2025-01-22 22:00:53 -0300

    Armazenar nomes e agregar amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo() {
+    const input = document.getElementById("amigo");
+    const nome = input.value.trim();
+
+    // Validação do Campo
+    if (nome === "") {
+        alert("Por favor, insira um nome válido.");
+        return;
+    }
+    // Adicionar o nome a lista
+    amigos.push(nome);
+    atualizarLista();
+
+    console.log(nome)
+
+    // Limpar o campo de entrada
+    input.value = "";
+}
